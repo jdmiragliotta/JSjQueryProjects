cccf5234f0f3ba2052fd461ea5cc670dd2422420
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-24 17:30:20 -0500

    Practiced with jQuery selectors

--- jQuerySlider/js/script.js
+++ jQuerySlider/js/script.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
-  $(".myLink").on("mouseenter",function(){
-    $("h1").addClass("red");
-  });
 
-  $(".myLink").on("mouseleave",function(){
-    $("h1").removeClass("red");
+  // $(".myLink").on("mouseenter",function(){
+  //   $("h1").addClass("red");
+  // });
+
+  // $(".myLink").on("mouseleave",function(){
+  //   $("h1").removeClass("red");
+  // });
+
+  // $(".myLink").click(function(){
+  //   $("h1").text("Hello World");
+  // });
+
+  // $(".myLink").click(function(){
+  //    $("h1").replaceWith("<h3>Hello World</h3>");
+  // });
+
+  // $(".myLink").click(function(){
+  //   $("h1").append("<h3>Hello World</h3>");
+  // });
+
+  $(".myLink").click(function(){
+    $("h1").css("font-size", "40px");
   });
 
   
